52dce32000fc0f36742b51e2bc84f2cb8ec98f97
Taku-pg <kaitsu505@gmail.com>
2025-12-17 15:14:41 +0100

    add pokemon-bush association with test

--- Models/Bush.cs
+++ Models/Bush.cs
@@ -1,15 +1,14 @@
-﻿
-namespace Models
+﻿namespace Models
 {
     [Serializable]
     public class Bush
     {
         private static List<Bush> _extent = new List<Bush>();
-        
+
         //Attributes
         private static double _encounterChance = 0.1;
         private bool _isActive;
-        
+
         public static double EncounterChance
         {
             get => _encounterChance;
@@ -19,64 +18,118 @@ namespace Models
                 {
                     throw new ArgumentException("Encounter chance must be between 0.0 and 1.0.");
                 }
+
                 _encounterChance = value;
             }
         }
-        
+
         public bool IsActive
         {
             get => _isActive;
             set => _isActive = value;
         }
-        
+
         //Associations
         private Road _road;
-        public Road Road 
-        { 
-            get => _road; 
-            set 
+
+        public Road Road
+        {
+            get => _road;
+            set { _road = value; }
+        }
+
+        private HashSet<Pokemon> _pokemons = new HashSet<Pokemon>();
+
+        public HashSet<Pokemon> GetPokemons() => new HashSet<Pokemon>(_pokemons);
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            if (_pokemons.Contains(pokemon))
             {
-                _road = value;
+                return;
+            }
+
+            bool added = false;
+
+            try
+            {
+                _pokemons.Add(pokemon);
+                added = true;
+                pokemon.AddBush(this);
+            }
+            catch (Exception e)
+            {
+                if (added)
+                {
+                    _pokemons.Remove(pokemon);
+                }
             }
         }
-        
-        public Bush() { }
+
+        public void RemovePokemon(Pokemon pokemon)
+        {
+            if (!_pokemons.Contains(pokemon))
+            {
+                return;
+            }
+
+            bool removed = false;
+
+            try
+            {
+                _pokemons.Remove(pokemon);
+                removed = true;
+                pokemon.RemoveBush(this);
+            }
+            catch (Exception e)
+            {
+                if (removed)
+                {
+                    _pokemons.Add(pokemon);
+                }
+            }
+        }
+
+        public Bush()
+        {
+        }
 
         public Bush(bool isActive, Road road)
         {
             if (road == null) throw new ArgumentNullException(nameof(road), "Bush must grow on a Road.");
-            
+
             IsActive = isActive;
-            
+
             Road = road;
             road.AddBush(this);
-            
+
             _extent.Add(this);
         }
-        
+
         public static List<Bush> GetExtent() => new List<Bush>(_extent);
 
         public static void Save(string path = "bushes.xml")
         {
             Serializer.Save(path, _extent);
         }
-        
+
         public static bool Load(string path = "bushes.xml")
         {
             var loadedList = Serializer.Load(path, _extent);
-        
+
             if (loadedList != null)
             {
                 _extent = loadedList;
                 return true;
             }
+
             return false;
         }
-        
+
         public static void RemoveFromExtent(Bush bush) => _extent.Remove(bush);
 
-        public void InvestigateBush() 
-        { 
+        public void InvestigateBush()
+        {
         }
     }
 }
